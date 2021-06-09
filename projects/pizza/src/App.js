85b75e2eb59b8a60f6af5e4a88b21d38bf8cf096
kyungsun choi <kschoi0308@gmail.com>
2021-06-09 20:47:24 +0900

    add pizza home

--- projects/pizza/src/App.js
+++ projects/pizza/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </>
   );
 }
