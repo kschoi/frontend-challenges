de37a687ac33344aab62086aef033f94554091f8
kyungsun choi <kschoi0308@gmail.com>
2021-06-10 13:28:04 +0900

    framer-motion variants1

--- projects/pizza-play/src/components/Base.js
+++ projects/pizza-play/src/components/Base.js
@@ -1,59 +1,84 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+};
 
 const Base = ({ addBase, pizza }) => {
-  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
-    <motion.div 
+    <motion.div
       className="container base"
-      initial={{ x: '100vw' }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', delay: 0.5 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
-          let spanClass = pizza.base === base ? 'active' : '';
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
           return (
-            // 2. hover 효과를 추가합니다.
-            <motion.li 
+            <motion.li
               key={base}
               onClick={() => addBase(base)}
-              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
-              transition={{ type: 'spring', stiffness: 300 }}
+              whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
+              transition={{ type: "spring", stiffness: 300 }}
             >
-              <span className={spanClass}>{ base }</span>
+              <span className={spanClass}>{base}</span>
             </motion.li>
-          )
+          );
         })}
       </ul>
 
       {pizza.base && (
         <motion.div
           className="next"
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-					transition={{ type: 'spring', stiffness: 120 }}
+          variants={nextVariants}
+          // initial="hidden" // initial, animate 이벤트 값은 자식 요소에게 전파됩니다.
+          // animate="visible"
         >
           <Link to="/toppings">
             <motion.button
-              // 1. hover 효과를 복붙합니다.
-		          whileHover={{ 
-		            scale: 1.1,
-		            textShadow: '0 0 8px rgb(255,255,255)',
-		            boxShadow: '0 0 8px rgb(255,255,255)',
-		          }}					
-						>
-							Next
-						</motion.button>
+              whileHover={{
+                scale: 1.1,
+                textShadow: "0 0 8px rgb(255,255,255)",
+                boxShadow: "0 0 8px rgb(255,255,255)",
+              }}
+            >
+              Next
+            </motion.button>
           </Link>
         </motion.div>
       )}
-
     </motion.div>
-  )
-}
+  );
+};
 
 export default Base;
