04ea463e82d0a5518a6e05f3b18db8c35aa410c2
kyungsun choi <kschoi0308@gmail.com>
2021-06-10 16:31:59 +0900

    add AnimatePresence

--- projects/pizza-play/src/components/Order.js
+++ projects/pizza-play/src/components/Order.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const containerVariants = {
   hidden: {
@@ -12,10 +12,10 @@ const containerVariants = {
     transition: {
       type: "spring",
       delay: 0.5,
-      mass: 0.4, // 2. 무게감이에요. 높은 값이면 느리다는 뜻, 낮은 값(0.1 시연)이면 빠르다는 뜻
-      damping: 8, // 3. 제동값이에요. 0부터 시연해보세요. 높은 값이면 빠르게 제동되고, 0이면 제동이 없습니다.
-      when: "beforeChildren", // 1. 자식요소 애니메이션이 발생하기 전에 애니메이션이 끝난다는 뜻
-      staggerChildren: 0.4, // 4. 자식요소들에 시차를 줘요. 시차의 시간값을 입력합니다.
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
     },
   },
 };
@@ -30,6 +30,14 @@ const childVariants = {
 };
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowTitle(false);
+    }, 4000);
+  }, []);
+
   return (
     <motion.div
       className="container order"
@@ -37,7 +45,11 @@ const Order = ({ pizza }) => {
       initial="hidden"
       animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2 exit={{ y: -1000 }}>Thank you for your order :)</motion.h2>
+        )}
+      </AnimatePresence>
       <motion.p variants={childVariants}>
         You ordered a {pizza.base} pizza with:
       </motion.p>
