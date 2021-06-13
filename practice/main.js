9c7c0969ce7ec17ee6dfc3a5badd12f48aa710a3
kschoi <kschoi0308@gmail.com>
2021-06-13 21:52:47 +0900

    navbar day2

--- practice/main.js
+++ practice/main.js
@@ -1,3 +1,12 @@
 "use strict";
 
-let a = 1;
+window.onload = function () {
+  const navbar = document.querySelector(".navbar");
+  const navbarToggle = document.querySelector(".navbar__toggle");
+
+  if (navbar && navbarToggle) {
+    navbarToggle.addEventListener("click", function () {
+      navbar.classList.toggle("active");
+    });
+  }
+};
